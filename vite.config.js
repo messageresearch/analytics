6cf1b10aeafc3be3d0602cc05d4fea74bca40cf0
messageanalytics <jp.calvo1@gmail.com>
2025-12-27 11:05:04 -0700

    fix: add public folder config and postbuild copy script to ensure data is deployed

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/wmbmentions.github.io/',
   plugins: [react()],
+  publicDir: 'public',
   server: { port: 5173 },
   build: { outDir: 'docs' }
 })
